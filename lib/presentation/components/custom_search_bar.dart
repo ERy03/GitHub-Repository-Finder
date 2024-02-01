51cbc677eefa4fb3649385cb80d06ccc0c9c5235
Ryota <ryoenoki2013@gmail.com>
2024-02-01 02:31:53 +0100

    add: CustomSearchBar

---
+++ lib/presentation/components/custom_search_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchBar extends StatefulWidget {
+  const CustomSearchBar({super.key});
+
+  @override
+  State<CustomSearchBar> createState() => _CustomSearchBarState();
+}
+
+class _CustomSearchBarState extends State<CustomSearchBar> {
+  final _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      child: SafeArea(
+        top: false,
+        bottom: false,
+        child: SearchBar(
+          controller: _controller,
+          padding: const MaterialStatePropertyAll<EdgeInsets>(
+              EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
+          leading: const Icon(Icons.search),
+          hintText: 'Search Repository',
+          onChanged: (text) => {
+            // TODO: search repository using GitHUb API
+          },
+        ),
+      ),
+    );
+  }
+}
