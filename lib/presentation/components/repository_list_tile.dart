a51ed32d777ce6be77b2a1a98314d741179ec80c
Ryota <ryoenoki2013@gmail.com>
2024-02-02 16:54:31 +0100

    update: include repository title

--- lib/presentation/components/repository_list_tile.dart
+++ lib/presentation/components/repository_list_tile.dart
@@ -9,22 +9,24 @@ class RepositoryListTile extends StatelessWidget {
         onTap: () {
           // TODO navigate to details page
         },
-        child: Container(
-          color: Colors.yellow,
+        child: SafeArea(
+          top: false,
+          bottom: false,
           child: Container(
-            decoration: BoxDecoration(
-                color: Colors.grey,
-                border: Border(
-                    bottom: BorderSide(
-                        color: Theme.of(context).dividerColor, width: 1))),
-            margin: const EdgeInsets.only(left: 16),
-            padding: const EdgeInsets.symmetric(vertical: 16),
-            child: Column(
-              children: <Widget>[
-                // heading (User logo and user name)
-                Padding(
-                  padding: const EdgeInsets.only(right: 16.0),
-                  child: Row(
+            color: Colors.yellow,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.grey,
+                  border: Border(
+                      bottom: BorderSide(
+                          color: Theme.of(context).dividerColor, width: 1))),
+              margin: const EdgeInsets.only(left: 16),
+              padding: const EdgeInsets.only(top: 16, bottom: 16, right: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  // heading (User logo and user name)
+                  Row(
                     children: <Widget>[
                       // TODO Replace container with actual image
                       Container(
@@ -44,14 +46,23 @@ class RepositoryListTile extends StatelessWidget {
                       )
                     ],
                   ),
-                ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  // repository title
+                  // TODO replace hardcoded text with actual repo title
+                  const Text(
+                    "Repository Title",
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  )
 
-                // repository title
+                  // repository description
 
-                // repository description
-
-                // number of stars and programming language
-              ],
+                  // number of stars and programming language
+                ],
+              ),
             ),
           ),
         ));
