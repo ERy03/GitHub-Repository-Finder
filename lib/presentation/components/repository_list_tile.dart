c6481f1fda61daa5e8787509c266130305c2716b
Ryota <ryoenoki2013@gmail.com>
2024-02-02 14:41:15 +0100

    wip: change styling

--- lib/presentation/components/repository_list_tile.dart
+++ lib/presentation/components/repository_list_tile.dart
@@ -10,40 +10,49 @@ class RepositoryListTile extends StatelessWidget {
           // TODO navigate to details page
         },
         child: Container(
-          color: Colors.grey,
-          child: Column(
-            children: <Widget>[
-              // heading (User logo and user name)
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Row(
-                  children: <Widget>[
-                    // TODO Replace container with actual image
-                    Container(
-                      height: 32,
-                      width: 32,
-                      decoration: const BoxDecoration(
-                          shape: BoxShape.circle, color: Colors.blue),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    const Expanded(
-                      child: Text(
-                        "Username",
-                        overflow: TextOverflow.ellipsis,
+          color: Colors.yellow,
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.grey,
+                border: Border(
+                    bottom: BorderSide(
+                        color: Theme.of(context).dividerColor, width: 1))),
+            margin: const EdgeInsets.only(left: 16),
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            child: Column(
+              children: <Widget>[
+                // heading (User logo and user name)
+                Padding(
+                  padding: const EdgeInsets.only(right: 16.0),
+                  child: Row(
+                    children: <Widget>[
+                      // TODO Replace container with actual image
+                      Container(
+                        height: 32,
+                        width: 32,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.blue),
                       ),
-                    )
-                  ],
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const Expanded(
+                        child: Text(
+                          "Username",
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              )
 
-              // repository title
+                // repository title
 
-              // repository description
+                // repository description
 
-              // number of stars and programming language
-            ],
+                // number of stars and programming language
+              ],
+            ),
           ),
         ));
   }
