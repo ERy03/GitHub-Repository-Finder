5dba8993287eda903b194423e5d40a70a4f62c74
Ryota <ryoenoki2013@gmail.com>
2024-02-06 02:34:29 +0100

    update: RepositoryDetailScreen

--- lib/presentation/screens/repository_detail_screen.dart
+++ lib/presentation/screens/repository_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:github_repository_finder/enums/enums.dart';
+import 'package:github_repository_finder/presentation/components/repository_detail_tile.dart';
 import 'package:github_repository_finder/presentation/components/repository_overview.dart';
 
 class RepositoryDetailScreen extends StatelessWidget {
@@ -8,12 +10,52 @@ class RepositoryDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Column(
-        children: [
-          RepositoryOverview(
-            isDetailScreen: true,
-          )
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const RepositoryOverview(
+              isDetailScreen: true,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            // repository detail
+            SafeArea(
+              top: false,
+              bottom: false,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color:
+                          Theme.of(context).primaryColorLight.withOpacity(0.1),
+                      border: Border.symmetric(
+                        horizontal: BorderSide(
+                          color: Theme.of(context).dividerColor,
+                          width: 0,
+                        ),
+                      )),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16),
+                    child: Column(children: [
+                      for (var details in RepositoryDetails.values)
+                        RepositoryDetailTile(
+                          label: details.label,
+                          icon: details.icon,
+                          color: details.color,
+                          licenseOrNumber: (
+                            license: 'MIT',
+                            number: 10000
+                          ), //TODO pass actual numbers
+                        )
+                    ]),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
