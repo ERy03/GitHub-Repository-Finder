e2462c9245253137b2ae105929e51077c8b52c02
Ryota <ryoenoki2013@gmail.com>
2024-02-08 00:00:47 +0100

    add: riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_repository_finder/presentation/screens/repositories_search_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
