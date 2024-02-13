431814f71f7949c83154b01c6b336b979a4b7f9e
Ryota <ryoenoki2013@gmail.com>
2024-02-13 17:31:13 +0100

    update: offset to menu anchor

--- lib/presentation/components/detail_screen/app_bar_popup_menu_button.dart
+++ lib/presentation/components/detail_screen/app_bar_popup_menu_button.dart
@@ -21,6 +21,7 @@ class _AppBarPopupMenuButtonState extends State<AppBarPopupMenuButton> {
   @override
   Widget build(BuildContext context) {
     return MenuAnchor(
+      alignmentOffset: const Offset(-175, 0),
       style: const MenuStyle(
         padding: MaterialStatePropertyAll(EdgeInsets.symmetric(vertical: 0)),
         shape: MaterialStatePropertyAll(RoundedRectangleBorder(
@@ -42,44 +43,51 @@ class _AppBarPopupMenuButtonState extends State<AppBarPopupMenuButton> {
         );
       },
       menuChildren: <Widget>[
-        MenuItemButton(
-          style: ButtonStyle(
-              fixedSize: const MaterialStatePropertyAll(Size(210, 55)),
-              shape: MaterialStatePropertyAll(LinearBorder.bottom(
-                  side: BorderSide(
-                color: Theme.of(context).dividerColor,
-                width: 0,
-              )))),
-          trailingIcon: Icon(
-            Icons.ios_share_outlined,
-            color: Theme.of(context).iconTheme.color,
-          ),
-          child: const Text(
-            'Share via...',
-          ),
-          onPressed: () async {
-            // sharedPosition is required for iPad
-            final box = context.findRenderObject() as RenderBox?;
-            await Share.share(
-              widget.htmlUrl,
-              subject:
-                  "Check out ${widget.repoAuthor}'s ${widget.repoTitle} repository",
-              sharePositionOrigin: box!.localToGlobal(Offset.zero) & box.size,
-            );
-          },
+        Column(
+          children: [
+            MenuItemButton(
+              style: const ButtonStyle(
+                minimumSize: MaterialStatePropertyAll(Size(210, 50)),
+              ),
+              trailingIcon: Icon(
+                Icons.ios_share_outlined,
+                color: Theme.of(context).iconTheme.color,
+              ),
+              child: const Text(
+                'Share via...',
+              ),
+              onPressed: () async {
+                // sharedPosition is required for iPad
+                final box = context.findRenderObject() as RenderBox?;
+                await Share.share(
+                  widget.htmlUrl,
+                  subject:
+                      "Check out ${widget.repoAuthor}'s ${widget.repoTitle} repository",
+                  sharePositionOrigin:
+                      box!.localToGlobal(Offset.zero) & box.size,
+                );
+              },
+            ),
+            const Divider(
+              height: 0,
+            ),
+            MenuItemButton(
+              style: const ButtonStyle(
+                minimumSize: MaterialStatePropertyAll(Size(210, 50)),
+              ),
+              trailingIcon: Icon(
+                Icons.public_outlined,
+                color: Theme.of(context).iconTheme.color,
+              ),
+              child: const Text(
+                'View on GitHub',
+              ),
+              onPressed: () {
+                gitHubLaunchUrl(widget.htmlUrl);
+              },
+            )
+          ],
         ),
-        MenuItemButton(
-          trailingIcon: Icon(
-            Icons.public_outlined,
-            color: Theme.of(context).iconTheme.color,
-          ),
-          child: const Text(
-            'View on GitHub',
-          ),
-          onPressed: () {
-            gitHubLaunchUrl(widget.htmlUrl);
-          },
-        )
       ],
     );
   }
