915159f0a7a7d636009cd3e5631ae7a7afb897b0
Ryota <ryoenoki2013@gmail.com>
2024-03-11 21:34:20 +0100

    update: main with easy localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_repository_finder/presentation/screens/repositories_search_screen.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ja')],
+      path: 'assets/translations',
+      startLocale: const Locale('ja'),
+      fallbackLocale: const Locale('en'),
+      child: const ProviderScope(child: MyApp())));
 }
 
 class MyApp extends StatelessWidget {
@@ -15,6 +23,9 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
         theme: ThemeData.dark(),
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
         home: const RepositoriesSearchScreen());
   }
 }
