7b303a40bc5075819a7cc891d3e03f580ac6e07f
Ryota <ryoenoki2013@gmail.com>
2024-03-26 16:40:48 +0100

    update: display error

--- lib/presentation/screens/repositories_search_screen.dart
+++ lib/presentation/screens/repositories_search_screen.dart
@@ -4,6 +4,8 @@ import 'package:github_repository_finder/data/github_pagination.dart';
 
 import 'package:github_repository_finder/data/github_repositories_repository.dart';
 import 'package:github_repository_finder/presentation/components/search_screen/no_github_repositories_found.dart';
+import 'package:github_repository_finder/presentation/components/shared/bot_padding.dart';
+import 'package:github_repository_finder/presentation/components/shared/error_messages.dart';
 import 'package:github_repository_finder/theme/theme_mode_provider.dart';
 import 'package:github_repository_finder/presentation/components/search_screen/custom_app_bar.dart';
 import 'package:github_repository_finder/presentation/components/search_screen/custom_search_bar.dart';
@@ -41,48 +43,62 @@ class RepositoriesSearchScreen extends ConsumerWidget {
                 child: query.isEmpty
                     ? const FindPrompt()
                     : ref.watch(gitHubRepositoryTotalCountProvider(query)).when(
-                        error: (e, _) => Center(child: Text(e.toString())),
-                        loading: () => const Center(
-                              child: CircularProgressIndicator.adaptive(),
+                        error: (e, _) {
+                          return BotPadding(
+                            child: ErrorMessages(
+                              error: e,
+                            ),
+                          );
+                        },
+                        loading: () => const BotPadding(
+                              child: Center(
+                                child: CircularProgressIndicator.adaptive(),
+                              ),
                             ),
                         data: (repositoriesCount) {
                           return repositoriesCount == 0
                               ? const NoGitHubRepositoriesFound()
                               : ListView.custom(
-                                  semanticChildCount: repositoriesCount,
                                   childrenDelegate: SliverChildBuilderDelegate(
                                       childCount: repositoriesCount,
                                       (context, index) {
-                                    // obtain page number (20 is the number of results obtain by calling the api)
-                                    final page = index ~/ 20 + 1;
-                                    // indexInPage is used to get the actual repository from the list
-                                    final indexInPage = index % 20;
+                                  // obtain page number (20 is the number of results obtain by calling the api)
+                                  final page = index ~/ 20 + 1;
+                                  // indexInPage is used to get the actual repository from the list
+                                  final indexInPage = index % 20;
 
-                                    final githubRepositories = ref.watch(
-                                        searchRepositoriesProvider(
-                                            pagination: GitHubPagination(
-                                                page: page, query: query)));
+                                  final githubRepositories = ref.watch(
+                                      searchRepositoriesProvider(
+                                          pagination: GitHubPagination(
+                                              page: page, query: query)));
 
-                                    return githubRepositories.when(
-                                        data: (gitHubResponse) {
-                                      return RepositoryListTile(
-                                        gitHubRepositoryModel:
-                                            gitHubResponse.items[indexInPage],
-                                      );
-                                    }, error: (e, _) {
-                                      if (indexInPage != 0) return null;
-                                      return Center(child: Text(e.toString()));
-                                    }, loading: () {
-                                      if (indexInPage != 0) return null;
-                                      return const Padding(
-                                        padding: EdgeInsets.only(top: 8.0),
-                                        child: Center(
-                                          child: CircularProgressIndicator
-                                              .adaptive(),
-                                        ),
-                                      );
-                                    });
-                                  }));
+                                  return githubRepositories.when(
+                                      data: (gitHubResponse) {
+                                    return RepositoryListTile(
+                                      gitHubRepositoryModel:
+                                          gitHubResponse.items[indexInPage],
+                                    );
+                                  }, error: (e, _) {
+                                    if (indexInPage != 0) return null;
+                                    return SafeArea(
+                                      top: false,
+                                      bottom: false,
+                                      child: Padding(
+                                          padding: const EdgeInsets.all(16.0),
+                                          child: ErrorMessages(error: e)),
+                                    );
+                                  }, loading: () {
+                                    if (indexInPage != 0) return null;
+                                    return const Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 8.0),
+                                      child: Center(
+                                        child: CircularProgressIndicator
+                                            .adaptive(),
+                                      ),
+                                    );
+                                  });
+                                }));
                         }))
           ],
         ),
